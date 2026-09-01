bef365221ccd933fb4e44606340572df8081d21b
tariqazizstacks <tariqaziz32492@gmail.com>
2026-09-01 23:30:47 +0500

    dom

---
+++ script.js
@@ -0,0 +1,9 @@
+// let heading=document.getElementById("heading")
+
+let abc= document.getElementByClassName("hd")
+
+let btn=document.querySelector("button")
+
+btn.addEventListener("click",()=>{
+    abc.innerText=" hhhhhhh"
+})
